22d31d82e781ca08d7af68efea150ab20e183696
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2017-12-18 00:09:19 +0100

    Toggle read boolean and icon

--- src/angular/components/itemDetail.js
+++ src/angular/components/itemDetail.js
@@ -1,4 +1,8 @@
 function ItemDetail($scope, $element, $attrs) {
+
+    this.toggleRead = (instance, read) => {
+        instance.read = read !== undefined ? read : !instance.read;
+    }
 }
 
 angular.module("aggregatorApp").component("itemDetail", {
